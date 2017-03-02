dcfe539acc1177dabe0354566cab371bb941f05b
冯振玲 <fengzhenling@wecash.net>
2017-03-02 19:38:56 +0800

    上传工程

---
+++ ReikoObject/ReikoObject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReikoObject
+//
+//  Created by 冯振玲 on 2017/2/28.
+//  Copyright © 2017年 Reiko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
